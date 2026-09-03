d9e2a9e4102589446afb5663e4e15acfaa4e9705
TDK-99 <vale.99.mezzo@gmail.com>
2026-09-03 02:37:47 +0200

    feat: add print for debug

--- src/ai_agents.py
+++ src/ai_agents.py
@@ -134,6 +134,11 @@ def agentic_analyze(jobs): # agentic ai that compare your cv with the output of 
     
     job_all= jobs_score
 
+    jobs_score.columns
+
+    print("response_list sample:", response_list[:2])
+    print("columns:", jobs_score.columns.tolist())
+
     jobs_score = jobs_score[jobs_score["score"]>=int(os.getenv("score_config"))]
 
     # df to dict
