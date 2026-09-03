c4ba1eb8c1d0d9418182dc8471bec1d5d1f52dcc
TDK-99 <vale.99.mezzo@gmail.com>
2026-09-03 13:27:13 +0200

    fix: if city in ai summ

--- src/ai_agents.py
+++ src/ai_agents.py
@@ -61,7 +61,7 @@ def agentic_summarize(jobs): # summirize the description and create an output of
 
     city =os.getenv("city").split(",")
 
-    if os.getenv("work_from_home") is None:
+    if os.getenv("city") is None:
         jobs
     else:
         jobs= jobs[jobs["city"].isin(city)]
