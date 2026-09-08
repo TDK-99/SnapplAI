ebc2dd69c3340e7bda56ff643219f4e226ef8d36
TDK-99 <vale.99.mezzo@gmail.com>
2026-09-08 14:20:49 +0200

    feat: add count value for print the after city filter on main

--- src/ai_agents.py
+++ src/ai_agents.py
@@ -177,12 +177,13 @@ def agentic_analyze(jobs): # agentic ai that compare your cv with the output of 
             job_all= jobs_score
         jobs_score = jobs_score[jobs_score["score"]>=int(os.getenv("score_config"))]
         jobs_score = jobs_score[["score", "location", "city", "company", "role", "work_mode", "a_summirize", "apply_link"]]
+        count_id =jobs_score["role"].count()
         jobs_score = jobs_score.to_dict(orient="records")
         jobs_score = json.dumps(jobs_score, indent=1)
         jobs_score = jobs_score.replace("'", "").replace("[", "").replace("]", "").replace("{", "").replace("},", "       ").replace('"', '').replace(',', '').replace('}\n', '')
 
         
-    return jobs_score, job_all
+    return jobs_score, job_all,count_id
 
 
 
